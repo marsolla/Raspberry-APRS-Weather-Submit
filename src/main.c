91a521a7f342b0ffc6148b1dbc6fe100da963027
marsolla <rafamarsolla@gmail.com>
2022-01-24 00:37:12 -0300

    Revert "removed temperature field from compressed packet"
    This reverts commit 2fee89870e2f5d5293768d8d3d946b1bc497228f.

--- src/main.c
+++ src/main.c
@@ -53,7 +53,7 @@ main (const int argc, const char** argv)
 {
 	char         packetToSend[BUFSIZE] = "";
 	char         packetFormat = UNCOMPRESSED_PACKET;
-	char         suppressUserAgent = 0;
+	char         suppressUserAgent = 1;
     char         c = '\0';          /* for getopt_long() */
     int          option_index = 0;  /* for getopt_long() */
 	int          i = 0;
